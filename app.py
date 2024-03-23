88f28628ee30bc57f92b45899a520285d8a03208
Raikevich Ivan <ivan.raykevich@mail.ru>
2024-03-23 15:56:21 +0300

    d

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    message = "Hello, World! (via Python)"
+    message = "Hello, World(via Python)"
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
