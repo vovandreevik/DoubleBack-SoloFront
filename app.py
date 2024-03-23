a1326f7d18ad503b775c9398e8648ecd4b4f3973
vovandreevik <vovandreevik@gmail.com>
2024-03-23 17:05:53 +0300

    back

--- app.py
+++ app.py
@@ -2,16 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-message = [
-    [{'name': 'John', 'age': 30, 'city': 'New York'}, {'name': 'Mary', 'age': 35, 'city': 'Los Angeles'}, {'name': 'Jane', 'age': 40, 'city': 'Chicago'}],
-    [{'name': 'Bob', 'age': 25, 'city': 'San Francisco'}, {'name': 'Sarah', 'age': 20, 'city': 'Dallas'}, {'name': 'Tom', 'age': 27, 'city': 'Seattle'}],
-    [{'name': 'Alice', 'age': 32, 'city': 'Boston'}, {'name': 'David', 'age': 37, 'city': 'Atlanta'}, {'name': 'Emma', 'age': 45, 'city': 'Miami'}]
-]
-
 
 @app.route('/')
 def hello_world():
-    
+    message = json_data
+
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
